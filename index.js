1161142dd5961f75893af10275771aa85fea0c86
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-06-23 22:19:54 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Follow along with the examples here
+function sayHello() {
+    console.log('Hello!');
+  }
+  sayHello();
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  } 
+  sayHelloToIsabel();
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+  sayHelloToSofia();
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  } 
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething('anything');
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo('Isabel'); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  } 
+  say("Hello", "Julio");
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  } 
+  function add(x, y) {
+    return x + y;
+  } 
+  add(1, 2)
+  console.log(add(80,9000));
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`
+  }
+  function subtract (x, y){
+      return x -y;
+  }
+  subtract(8, 5)
+  console.log(subtract(8,5));
